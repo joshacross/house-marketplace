fa2f0e125bfe6b9f8f1e270a30c0cdeac5e06c8a
Joshua Cross <74011006+joshacross@users.noreply.github.com>
2022-11-05 18:47:28 -0500

    initializing offers page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,7 +56,7 @@ function Navbar() {
                                              ? 'navbarListItemNameActive'
                                              : 'navbarListItemName'
                                    }>
-                                   Offer
+                                   Offers
                               </p>
                          </li>
                          <li
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,9 +1,93 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+     collection,
+     getDocs,
+     query,
+     where,
+     orderBy,
+     limit,
+     startAfter,
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
 
 function Offers() {
+     const [listings, setListings] = useState(null);
+     const [loading, setLoading] = useState(true);
+
+     const params = useParams();
+
+     useEffect(() => {
+          const fetchListings = async () => {
+               try {
+                    // get a reference
+                    const listingsRef = collection(db, 'listings');
+
+                    // create a query
+                    const q = query(
+                         listingsRef,
+                         where('offer', '==', true)
+                         // orderBy('timeStamp', 'desc'),
+                         // limit(10)
+                    );
+
+                    // execute query to get a "snapshot"
+                    const querySnap = await getDocs(q);
+
+                    // have to loop through the getDocs
+                    // take the querySnapshot and loop through using forEach
+
+                    const listings = [];
+
+                    querySnap.forEach((doc) => {
+                         console.log('doc.data() =>', doc.data());
+                         return listings.push({
+                              id: doc.id,
+                              data: doc.data(),
+                         });
+                    });
+                    setListings(listings);
+                    setLoading(false);
+               } catch (error) {
+                    toast.error('Could not fetch listings');
+               }
+          };
+
+          fetchListings();
+     }, [params.categoryName]);
+
      return (
-          <div>
-               <h1>Offers</h1>
+          <div className='category'>
+               <header>
+                    <p className='pageHeader'>
+                         {params.categoryName === 'rent'
+                              ? 'Places for Rent'
+                              : 'Places for sale'}
+                    </p>
+               </header>
+
+               {loading ? (
+                    <Spinner />
+               ) : listings && listings.length > 0 ? (
+                    <>
+                         <main>
+                              <ul className='categoryListings'>
+                                   {listings.map((listing) => (
+                                        <ListingItem
+                                             listing={listing.data}
+                                             id={listing.id}
+                                             key={listing.id}
+                                        />
+                                   ))}
+                              </ul>
+                         </main>
+                    </>
+               ) : (
+                    <p>No listings for {params.categoryName}</p>
+               )}
           </div>
      );
 }
